fb0b5355f9404cdbcebf8e4aba75d12ac280ce0a
Maksym Siroshtan <siroshtan.max1991UA@gmail.com>
2022-08-15 20:56:43 +0300

    Модифицировал файл js

--- scripts/burger-menu-pop-up.js
+++ scripts/burger-menu-pop-up.js
@@ -2,12 +2,13 @@ const burgerMenuOpen = document.querySelector('#burger-menu-link');
 const burgerPopUpOpen = document.querySelector('#burger__pop-up');
 const burgerPopUpClose = document.querySelector('#burger__pop-up-close');
 
-burgerMenuOpen.addEventListener('click', function ready() {
-    burgerPopUpOpen.style.display = 'flex';
-});
-
-burgerPopUpClose.addEventListener('click', function ready() {
-    burgerPopUpOpen.style.display = 'none';
-});
-
-document.addEventListener('DOMContentLoaded', ready);
+document.addEventListener("DOMContentLoaded", function(event) { 
+    burgerMenuOpen.addEventListener('click', function(event) {
+        burgerPopUpOpen.style.display = 'flex';
+    });
+    
+    burgerPopUpClose.addEventListener('click', function(event) {
+        burgerPopUpOpen.style.display = 'none';
+    });
+    
+  });
